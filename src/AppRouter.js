2bb6a1ae71827e412b02664b0655bb5b3ff35479
sreejith puthenveettil <sreejithputhenveettil@sysadms-MacBook-Pro-4.local>
2020-04-16 02:28:37 +0530

    Shoping cart react app

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
+import { useLocation } from 'react-router-dom'
 
 import Home from './Pages/Home';
 import Cart from './Pages/Cart';
 
+
 const AppRouter = (
-	<Router>
+
+<Router>
 		<Switch>
+		<BrowserRouter basename={window.location.pathname || ''}>
 			<Route path='/' exact component={Home} />
 			<Route path='/cart' component={Cart} />
+		</BrowserRouter>
 		</Switch>
 	</Router>
 );
