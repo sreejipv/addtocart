f360a9edd10dd4f87381df7dfdc64cecaceb2d3e
sreejith puthenveettil <sreejithputhenveettil@sysadms-MacBook-Pro-4.local>
2020-04-17 13:34:41 +0530

    modified slider

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -14,7 +14,9 @@ const [max] = useState(100)
 const [min] = useState(1)
 const [gapValue, setGapValue] = useState(0)
 const { setRange } = useContext(FilterContext)
-
+const [newWidth, setNewWidth] = useState(0)
+const[newMarginLeft, setNewMarginLeft] = useState(0)
+const [newPos, setNewPos] = useState(0)
 
 function handleLeftrange(e){
     let value = parseInt(e.target.value);
@@ -25,8 +27,8 @@ function handleLeftrange(e){
         setLeftvalue(value)        
     }
 
-    const newWidth = `calc(${100 - leftvalue - (max-rightvalue)}%)`
-    const newMarginLeft = `calc(${leftvalue}%)`
+    setNewWidth(`calc(${100 - leftvalue - (max-rightvalue)}%)`)
+    setNewMarginLeft(`calc(${leftvalue}%)`)
 
     if(rightvalue-leftvalue === 10) {
       setGapValue(2)
@@ -34,8 +36,9 @@ function handleLeftrange(e){
       setGapValue(0)
     }
 
-    const newPos = `calc(${leftvalue}% + (${8 -(leftvalue * 0.750) - gapValue}px))`
+    setNewPos(`calc(${leftvalue-16}% + (${8 -(leftvalue * 0.30)}px))`)
 
+    console.log(value)
     filterValueLeft.current.style.width = newWidth
     filterValueLeft.current.style.marginLeft = newMarginLeft
     filterValueStart.current.style.left = newPos
@@ -55,7 +58,7 @@ function handleRightrange(e){
     filterValueEnd.current.style.left = newPos
 }
 function setFilter() {
-  setRange({left: leftvalue *100, right: rightvalue *100})
+  setRange({left: leftvalue *1000, right: rightvalue *1000})
 }
 
 const styledValueLeft = {
@@ -64,7 +67,7 @@ const styledValueLeft = {
 }
 const styledValueStart = {
   position: 'absolute',
-  left: '0px',
+  left: '-14%',
 }
 const styledValueEnd = {
   position: 'absolute',
@@ -75,13 +78,13 @@ const styledValueEnd = {
         <p className="tLeft fBold desktop-only">Filters</p>
         <p className="mobile-only tLeft fBold pl-15">Filter Options</p>
         <div className="multi-range pt-10">
-                  <input ref={textInput1} className="filter lower" type="range" min={min} max={max} value={leftvalue} onChange={ handleLeftrange } />
-                  <input ref={textInput2} className="filter upper" type="range" min={min} max={max} value={rightvalue} onChange={ handleRightrange} />
+                  <input ref={textInput1} className="filter lower" type="range" min={min} step="1" max={max} value={leftvalue} onChange={ handleLeftrange } />
+                  <input ref={textInput2} className="filter upper" type="range" min={min} step="1"  max={max} value={rightvalue} onChange={ handleRightrange} />
                   <div className="rangeSlider">
                     <div ref={filterValueLeft}  style={styledValueLeft} className="rangeSlider--range"></div>
                   </div>
-                  <span ref={filterValueStart}  style={styledValueStart} className="startPoint">{leftvalue *100}</span>
-                  <span ref={filterValueEnd} style={styledValueEnd}  className="endPoint">{rightvalue *100}</span>
+                  <span ref={filterValueStart}  style={styledValueStart} className="startPoint">{leftvalue *1000}</span>
+                  <span ref={filterValueEnd} style={styledValueEnd}  className="endPoint">{rightvalue *1000}</span>
           </div>
           <button className="secondary-btn fBold cPointer desktop-only" onClick={setFilter}>
               Apply
