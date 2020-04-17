bc22904b43692767cd23ca538dc2d9b62d60995c
sreejith puthenveettil <sreejithputhenveettil@sysadms-MacBook-Pro-4.local>
2020-04-17 15:21:09 +0530

    modified slider

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -3,67 +3,60 @@ import ProductsDataContext from '../Context/ProductsContext'
 import FilterContext from '../Context/FilterContext'
 
 const Filter = ({ parentCallback }) => {
-const textInput1 = useRef(null);
-const textInput2 = useRef(null);
+// const textInput1 = useRef(null);
+// const textInput2 = useRef(null);
 const filterValueLeft = useRef(null);
 const filterValueStart = useRef(null);
 const filterValueEnd = useRef(null);
 const [leftvalue , setLeftvalue] = useState(1)
 const [rightvalue , setRightvalue] = useState(10)
-const [max] = useState(100)
+const [max] = useState(300)
 const [min] = useState(1)
 const [gapValue, setGapValue] = useState(0)
 const { setRange } = useContext(FilterContext)
-const [newWidth, setNewWidth] = useState(0)
-const[newMarginLeft, setNewMarginLeft] = useState(0)
-const [newPos, setNewPos] = useState(0)
+
 
 function handleLeftrange(e){
     let value = parseInt(e.target.value);
-
     if(value > rightvalue - 10) {
         setLeftvalue(rightvalue - 10)
     }else{
         setLeftvalue(value)        
     }
-
-    setNewWidth(`calc(${100 - leftvalue - (max-rightvalue)}%)`)
-    setNewMarginLeft(`calc(${leftvalue}%)`)
+    const newWidth = `calc(${(100 - leftvalue/3 - (max/3-rightvalue/3))}%)`
+    const newMarginLeft = `calc(${(leftvalue/3)}% + (${8 -(leftvalue/3 * 0.15)}px))`
 
     if(rightvalue-leftvalue === 10) {
       setGapValue(2)
     } else {
       setGapValue(0)
     }
-
-    setNewPos(`calc(${leftvalue-16}% + (${8 -(leftvalue * 0.30)}px))`)
-
-    console.log(value)
+    const newPos = `calc(${(leftvalue/3)-22}% + (${8 -(leftvalue/3 * 0.30)}px))`
     filterValueLeft.current.style.width = newWidth
     filterValueLeft.current.style.marginLeft = newMarginLeft
     filterValueStart.current.style.left = newPos
+ 
 }
 function handleRightrange(e){
     let value = parseInt(e.target.value);
- 
-    if(value < leftvalue + 10) {
-        setRightvalue(leftvalue + 10)
+
+    if(value < leftvalue + 1) {
+        setRightvalue(leftvalue + 1)
     }else{
         setRightvalue(value)
     }
-
-    const newWidth = `calc(${100 - leftvalue - (max-rightvalue)}%)`
-    filterValueLeft.current.style.width = newWidth
-    const newPos = `calc(${rightvalue}% + (${8 - rightvalue * 0.30}px))`
+    const newPos = `calc(${(rightvalue/3)}% + (${8 -(leftvalue/3 * 0.20)}px) )`
     filterValueEnd.current.style.left = newPos
+
 }
+
 function setFilter() {
   setRange({left: leftvalue *1000, right: rightvalue *1000})
 }
 
 const styledValueLeft = {
-  width: `calc(${100 - leftvalue - (max-rightvalue)}%)`,
-  marginLeft: '0px'
+  width: `calc(${100 - (leftvalue/3) - ((max/3)-(rightvalue/3))}%)`,
+  marginLeft: '11px'
 }
 const styledValueStart = {
   position: 'absolute',
@@ -78,8 +71,8 @@ const styledValueEnd = {
         <p className="tLeft fBold desktop-only">Filters</p>
         <p className="mobile-only tLeft fBold pl-15">Filter Options</p>
         <div className="multi-range pt-10">
-                  <input ref={textInput1} className="filter lower" type="range" min={min} step="1" max={max} value={leftvalue} onChange={ handleLeftrange } />
-                  <input ref={textInput2} className="filter upper" type="range" min={min} step="1"  max={max} value={rightvalue} onChange={ handleRightrange} />
+                  <input className="filter lower" type="range" min={min}  max={max} value={leftvalue} onChange={ handleLeftrange } />
+                  <input className="filter upper" type="range" min={min}  max={max} value={rightvalue} onChange={ handleRightrange} />
                   <div className="rangeSlider">
                     <div ref={filterValueLeft}  style={styledValueLeft} className="rangeSlider--range"></div>
                   </div>
